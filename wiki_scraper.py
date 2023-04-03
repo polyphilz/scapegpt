56e173c5383c234f974a9d9c3c8d7f0fcad55d90
Rohan Bansal <rohanarunbansal@gmail.com>
2023-04-02 21:12:13 -0700

    Refactor scraping logic and add dedicated file for collected wiki slugs as well as dedicated directory for storing output summaries (pre-embeddings)

--- wiki_scraper.py
+++ wiki_scraper.py
@@ -1,88 +1,81 @@
 import requests
+
 from bs4 import BeautifulSoup
+from collections import OrderedDict
 from datetime import datetime
 
-# Send a GET request
-url = "https://oldschool.runescape.wiki/w/Kourend_map_display_case"
-res = requests.get(url)
 
-# Parse HTML content using BeautifulSoup
-soup = BeautifulSoup(res.content, "html.parser")
-
-# Get the page title
-title = soup.find("h1", id="firstHeading").text.strip()
-
-# Get the content section
-content_section = soup.find("div", id="bodyContent").find("div", id="mw-content-text")
-
-# Get the text description of the item
-description = (
-    content_section.find("div", class_="mw-parser-output").find("p").text.strip()
-)
-
-# Find the table element
-content_tables = soup.find_all('table', {'class': 'wikitable'})
-
-# Extract the data from the table rows
-rows = table.find_all('tr')
-data = []
-for row in rows[1:]:
-    cols = row.find_all('td')
-    badge_image = cols[0].find('img')['src']
-    badge_name = cols[1].get_text().strip()
-    badge_level = cols[2].get_text().strip()
-    badge_xp = cols[3].get_text().strip()
-    data.append((badge_image, badge_name, badge_level, badge_xp))
-
-# Format the data as markdown and write to a file
-with open('x.md', 'w') as f:
-    f.write('# Badge Summary\n\n')
-    f.write('| Badge | Level | Steam XP |\n')
-    f.write('|-------|-------|----------|\n')
-    for row in data:
-        f.write(f'| ![badge]({row[0]}) | {row[1]} | {row[2]} | {row[3]} |\n')
+OSRS_WIKI_URL_BASE = "https://oldschool.runescape.wiki/w/"
+SLUGS_FILE = "slugs.txt"
+SUMMARIES_DIR = "summaries/"
 
 
+def get_url_slugs():
+    url_slugs = []
+    with open(SLUGS_FILE, "r") as file:
+        for line in file:
+            url_slugs.append(line.strip())
+    return url_slugs
 
 
-# Extract table data
-table = soup.find("table", {"class": "infobox"})
+def main():
+    url_slugs = get_url_slugs()
 
-# Get the release date from the infobox table
-rows = table.find_all("tr")
-info = {}
-release_date = ""
-for row in rows:
-    cols = row.find_all(["th", "td"])
-    if len(cols) == 2:
-        if cols[0].text.strip() == "Released":
-            release_date = (
-                cols[1].text.strip().replace(" (Update)", "").replace("(Update)", "")
-            )
-        else:
-            info[cols[0].text.strip()] = cols[1].text.strip()
+    for slug in url_slugs:
+        url = OSRS_WIKI_URL_BASE + slug
+        res = requests.get(url)
+
+        # Parse HTML content of article using BeautifulSoup
+        soup = BeautifulSoup(res.content, "html.parser")
+
+        # Get the article title
+        title = soup.find("h1", id="firstHeading").text.strip()
+
+        # Get the article content
+        content_section = soup.find("div", id="bodyContent").find(
+            "div", id="mw-content-text"
+        )
+        description = (
+            content_section.find("div", class_="mw-parser-output")
+            .find("p")
+            .text.strip()
+        )
+
+        # Get the article info box
+        infobox_table = soup.find("table", {"class": "infobox"})
+        rows = infobox_table.find_all("tr")
+        info = OrderedDict()
+        for row in rows:
+            cols = row.find_all(["th", "td"])
+
+            # Skip rows that don't have heading labels with information. For
+            # example, the first and second rows of the infobox table contain
+            # the article title and associated image; those can be skipped.
+            if len(cols) < 2:
+                continue
+
+            row_label = cols[0].text.strip()
+            row_content = cols[1].text.strip()
+            if row_label == "Released":
+                row_content = row_content.replace(" (Update)", "").replace(
+                    "(Update)", ""
+                )
+            info[row_label] = row_content
+
+        infobox_info = ""
+        for info_label, info_content in info.items():
+            infobox_info += f"{info_label}: {info_content}\n"
+
+        # Get the output
+        output = f"# {title}\n\n## Content\n\n{description}\n\n## Information\n\n{infobox_info}"
+
+        # Save the output to a file
+        filename = title.lower().replace(" ", "-") + ".md"
+        with open(SUMMARIES_DIR + filename, "w", encoding="utf-8") as f:
+            f.write(output)
+
+        print(f"Output saved to {filename}")
 
 
-
-
-
-# Generate markdown output
-output = f"# {title}\n\n## Content\n\n{description}\n\n## Information\n\n"
-if release_date:
-    output += f"{title} was released on {release_date}, "
-else:
-    output += f"{title} does not have a release date, "
-output += f"is {'members-only' if info.get('Members', 'Yes') == 'Yes' else 'not members-only (it is accessible by free-to-play players)'}, "
-if info["Quest"] == "No":
-    output += "does not belong to any quest, "
-else:
-    output += f"belongs to the quest {info['Quest']}, "
-output += f"is located in {info.get('Location', 'N/A')}, has {'no' if info.get('Options', 'N/A') == 'None' else ''} options, "
-output += f"and has the examine text '{info.get('Examine', 'N/A')}'.\n"
-
-# Save the output to a file
-filename = title.lower().replace(" ", "-") + ".md"
-with open(filename, "w", encoding="utf-8") as f:
-    f.write(output)
-
-print(f"Output saved to {filename}")
+if __name__ == "__main__":
+    main()
